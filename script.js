f96433bae0911264facc793ffa7ccb750e258ef4
Andika <litageming@gmail.com>
2023-02-08 11:48:34 +0700

    jk

--- script.js
+++ script.js
@@ -10,12 +10,3 @@ menubar.onclick = () =>{
 console.log("punya trisia")
 
 
-//tempat menampilkan foto-foto
-document.addEventListener("click",function (e){
-    if(e.target.classList.contains("gallery-item")){
-        const src = e.target.getAttribute("src");
-        document.querySelector(".modal-img").src = src;
-        const myModal = new bootstrap.Modal(document.getElementById('gallery-popup'));
-        myModal.show();
-    }
-  })
